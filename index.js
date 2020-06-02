96bb78e43687516d4bc012be66e234a969097d9c
vasw0001 <65926478+vasw0001@users.noreply.github.com>
2020-06-01 21:59:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+//TASK1
+let a = 3;
+let b = 5;
+let c;
+
+let result;
+
+// task description
+result = 'let a = 3;\n';
+result += 'let b = 5;\n';
+result += 'let c;\n';
+result += '---------\n';
+
+// using operators
+result += 'a + b = ' + (a + b) + '\n';
+result += 'a - b = ' + (a - b) + '\n';
+result += 'a * b = ' + (a * b) + '\n';
+result += 'a / b = ' + (a / b) + '\n';
+result += 'a % b = ' + (a % b) + '\n';
+result += 'a += b = ' + (a += b) + '\n';
+result += 'a -= b = ' + (a -= b) + '\n';
+result += 'a *= b = ' + (a *= b) + '\n';
+result += 'a /= b = ' + (a /= b) + '\n';
+result += 'a %= b = ' + (a %= b) + '\n';
+result += 'a == b : ' + (a == b) + '\n';
+result += 'a != b : ' + (a != b) + '\n';
+result += 'a > b : ' + (a > b) + '\n';
+result += 'a < b : ' + (a < b) + '\n';
+result += '!a && !c : ' + (!a && !c) + '\n';
+result += '!a || !c : ' + (!a || !c) + '\n';
+
+//printing results
+alert(result);
+
+
+//TASK2
+//declaring variables
+let first_name = "Renita";
+let last_name = "Vaswani";
+let email = "vaswanirenita@gmail.com";
+let output;
+
+//concatenating variables
+output = 'My name is ' + first_name + ' ' + last_name + '. You can contact me at ' + email + '.'
+
+//displaying concatenation
+console.log(output); 
+
+
+
+
+
+
